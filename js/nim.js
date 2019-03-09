6e3afd34ba55a54613024bdca6ed2304b325e964
Marianne Lyssand <mariannelyssand@Mariannes-MacBook-Pro.local>
2019-03-09 09:41:46 +0100

    fått inn human egenskapen til player 1 & player 2, og 2 til if'er, i tilfelle det ikke blir skrevet inn navn.

--- js/nim.js
+++ js/nim.js
@@ -7,13 +7,29 @@ function Nim(player1, player2, victory, total, maxGrab) {
 
   if (player1 != undefined) {
     this.player1 = {
-      name: player1
+      name: player1,
+      human: true
+    }
+  }
+  
+  if (player1 === undefined) {
+    this.player = {
+      name: Computer1,
+      human: false
     }
   }
 
   if (player2 != undefined) {
     this.player2 = {
-      name: player2
+      name: player2,
+      human = true
+    }
+  }
+
+  if (player2 === undefined) {
+    this.player = {
+      name: Computer2,
+      human: false
     }
   }
 
