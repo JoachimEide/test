96d939fd7c6bedefd82f0c6d464157a4545df794
Joachim Eide <JoachimEide@Joachims-MacBook-Pro-2.local>
2019-03-11 00:32:49 +0100

    er2

--- js/game.js
+++ js/game.js
@@ -1,5 +1,5 @@
 const player1 = document.getElementById(navn1);
 const player2 = document.getElementById(navn2);
 const check1 = document.getElementById(check1);
-const check1 = document.getElementById(check2);
+const check2 = document.getElementById(check2);
 const game = Nim(player1, player2, undefined, undefined, undefined);
