6380e6e937ada5dad23988b32943d5b76de97120
Joachim Eide <JoachimEide@Joachims-MacBook-Pro-2.local>
2019-03-11 00:44:45 +0100

    Forsøk på å hente id'er til variabler

--- js/game.js
+++ js/game.js
@@ -1,5 +1,21 @@
-const player1 = document.getElementById(navn1);
-const player2 = document.getElementById(navn2);
-const check1 = document.getElementById(check1);
-const check2 = document.getElementById(check2);
+const getPlayer1 = () => {
+  let player1 = document.getElementById(navn1);
+  return player1;
+}
+
+const getPlayer2 = () => {
+  let player2 = document.getElementById(navn2);
+  return player2;
+}
+
+const humanOrMachine1 = () => {
+  let check1 = document.getElementById(check1);
+  return check1;
+}
+
+const humanOrMachine2 = () => {
+  let check2 = document.getElementById(check2);
+  return check2;
+}
+
 const game = Nim(player1, player2, undefined, undefined, undefined);
