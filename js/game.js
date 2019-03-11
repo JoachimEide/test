1f918a73e5bbb913ba3f65c5378b30b7b0cfba44
Marianne Lyssand <mariannelyssand@pc-104-120.eduroam.uib.no>
2019-03-11 13:51:06 +0100

    startspill test

--- js/game.js
+++ js/game.js
@@ -1,3 +1,6 @@
+document.getElementById("startSpill").onclick = function startSpill () {
+  location.href = "game.html";
+
 const getPlayer1 = () => {
   let player1 = document.getElementById("player1").value;
   console.log(`Player 1 med navn ${player1} er opprettet!`);
