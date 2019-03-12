96d0fa1361b05e715d0e74557294388533f78cb7
Joachim Eide <JoachimEide@Joachims-MacBook-Pro-2.local>
2019-03-12 09:37:34 +0100

    game

--- js/game.js
+++ js/game.js
@@ -1,26 +1,5 @@
-const getPlayer1 = () => {
-  let player1 = document.getElementById("player1").value;
-  console.log(`Player 1 med navn ${player1} er opprettet!`);
-  return player1;
-}
 
-const getPlayer2 = () => {
-  let player2 = document.getElementById("player2").value;
-    console.log(`Player 2 med navn ${player2} er opprettet!`);
-    return player2;
-}
-
-const humanOrMachine1 = () => {
-  let check1 document.getElementById("check1").checked;
-  return check1;
-}
-
-const humanOrMachine2 = () => {
-  let check2 = document.getElementById("check2").checked;
-  return check2;
-}
-
-function getInfo(id){
+const getInfo => (id) {
   var info = document.getElementById(id).value
 
   if (info == "") {
@@ -29,11 +8,17 @@ function getInfo(id){
   return info
 }
 
-function samlefunksjon() {
-  var spiller1 = getInfo("player1")
-  var spiller2 = getInfo("navn2")
-  var liste = [spiller1, spiller2]
-  return liste
+const humanOrMachine = (check) => {
+  let check = document.getElementById(check).checked;
+  return check1;
+}
 
 
+const allInfo = () => {
+  const spiller1 = getInfo("player1");
+  const spiller2 = getInfo("player2");
+  const check1 = humanOrMachine("check1");
+  const check2 = humanOrMachine("check2");
+  const listOfAll = [spiller1, check1, spiller2, check2];
+  return listOfAll;
 }
