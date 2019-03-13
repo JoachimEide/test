9c4b741fb93d469319876537f1d511c05b06efa8
Marianne Lyssand <mariannelyssand@Mariannes-MacBook-Pro.local>
2019-03-13 11:33:47 +0100

    lagt inn 1 player vs computer

--- js/nim.js
+++ js/nim.js
@@ -48,6 +48,11 @@ function Nim(player1, player2, victory, total, maxGrab) {
   if (this.player1.human === false && this.player2.human === false) {
     throw 'Minimum en av spillerne må være human!';
   }
+  
+  // 1 player vs computer
+  if (this.player1.name != undefined && this.player2.name === undefined) {
+    this.player2.name = 'Computer';
+  }
 
 //Total
   if (total === undefined) {
