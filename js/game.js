bf2e90e03bed2be9617e413a5e41e1b9e6c0a40f
Joachim Eide <JoachimEide@Joachims-MacBook-Pro-2.local>
2019-03-14 15:36:29 +0100

    check og rekursjon i game

--- js/game.js
+++ js/game.js
@@ -37,7 +37,7 @@ const getUserData = () => {
   console.log(gameObject)
   makeGame(userData[3], userData[2])
   let turnCount = 1;
-  buttonAction(gameObject.total, turnCount);
+  buttonAction(gameObject.total, turnCount, gameObject.player1.name, gameObject.player2.name,);
   return console.log(gameObject);
   }
 
@@ -54,70 +54,69 @@ const makeGame = (maxGrab, totalCounter) => {
   }
 }
 
-const checkTurn = (turn) => {
+const checkTurn = (turn, p1, p2) => {
   let message = "";
   if (turn % 2 != 0) {
-    message = `Det er ${gameObject.player1.name} sin tur:`
+    message = `Det er ${p1} sin tur:`
     document.getElementById("message").innerHTML = message;
-    turn++;
   } else {
-    message = `Det er ${gameObject.player2.name} sin tur:`
+    message = `Det er ${p2} sin tur:`
     document.getElementById("message").innerHTML = message;
-    turn++;
   }
 }
 
-const buttonAction = (total, turn) => {
+const buttonAction = (total, turn, p1, p2) => {
   const listOfButtonID = ["p1-1","p2-1","p1-2","p2-2","p1-3","p2-3","p1-4","p2-4"];
+  checkTurn(turn, p1, p2);
   if (turn % 2 != 0) {
       document.getElementById(listOfButtonID[0]).addEventListener("click", function(){
       total -= 1;
       turn++;
       document.getElementById("total").innerHTML = total;
-      buttonAction(total, turn);
+      buttonAction(total, turn, p1, p2);
     },{once: true})
       document.getElementById(listOfButtonID[2]).addEventListener("click", function(){
       total -= 2;
       turn++;
       document.getElementById("total").innerHTML = total;
-      buttonAction(total, turn);
+      buttonAction(total, turn, p1, p2);
     },{once: true})
     document.getElementById(listOfButtonID[4]).addEventListener("click", function(){
       total -= 3;
       turn++;
       document.getElementById("total").innerHTML = total;
-      buttonAction(total, turn);
+      buttonAction(total, turn, p1, p2);
     },{once: true})
     document.getElementById(listOfButtonID[6]).addEventListener("click", function(){
       total -= 4;
       turn++;
       document.getElementById("total").innerHTML = total;
-      buttonAction(total, turn);
+      buttonAction(total, turn, p1, p2);
     },{once: true})
   } else {
     document.getElementById(listOfButtonID[1]).addEventListener("click", function(){
       total -= 1;
       turn--;
       document.getElementById("total").innerHTML = total;
-      buttonAction(total, turn);
+      buttonAction(total, turn, p1, p2);
     },{once: true})
     document.getElementById(listOfButtonID[3]).addEventListener("click", function(){
       total -= 2;
       turn--;
       document.getElementById("total").innerHTML = total;
-      buttonAction(total, turn);
+      buttonAction(total, turn, p1, p2);
     },{once: true})
     document.getElementById(listOfButtonID[5]).addEventListener("click", function(){
       total -= 3;
       turn--;
       document.getElementById("total").innerHTML = total;
-      buttonAction(total, turn);
+      buttonAction(total, turn, p1, p2);
     },{once: true})
     document.getElementById(listOfButtonID[7]).addEventListener("click", function(){
       total -= 4;
       turn--;
       document.getElementById("total").innerHTML = total;
-      buttonAction(total, turn);
+      buttonAction(total, turn, p1, p2);
     },{once: true})
   }
   }
